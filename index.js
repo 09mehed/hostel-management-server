a8f9ac5848fee333f11834d6a241a55c7d18dc40
09mehed <foysalalmamun558@gmail.com>
2025-02-19 19:14:24 +0600

    website is running

--- index.js
+++ index.js
@@ -467,13 +467,6 @@ async function run() {
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '365d',
       })
-      // res
-      //   .cookie('token', token, {
-      //     httpOnly: true,
-      //     secure: process.env.NODE_ENV === 'production',
-      //     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-      //   })
-      //   .send({ success: true })
       res.send({ token })
     })
     // Logout
